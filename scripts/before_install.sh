22349c6fe7f2233727e2befebe018b5f015d4cd4
alienx456 <estebanelias27@gmail.com>
2020-06-13 22:33:54 -0500

    Fixed appspec

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,6 @@
 echo 'installing nodejs...'
 apt update
 apt install python-software-properties
-#curl -sL https://deb.nodesource.com/setup_14.x | bash -
-#apt install -y nodejs
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt install -y nodejs
 echo '---------------before_install.sh Terminated---------------'
