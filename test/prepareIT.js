082345a542f66d3e70bc635b76d806dcb9c05761
alienx456 <estebanelias27@gmail.com>
2020-06-28 15:20:09 -0500

    Fixed sintax

--- test/prepareIT.js
+++ test/prepareIT.js
@@ -10,4 +10,4 @@ var myerror = null;
 pool.query("DELETE FROM employee_schema.employee")
     .then(result => console.log('Data succesfully removed for testing'))
     .then(e => console.log(e))
-    .finally(() => {pool.end())
+    .finally(() => pool.end())
